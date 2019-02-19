35e57459a1fa5eedd3b3eaba195872558c16376b
gadjetron <gi87365@yandex.ru>
2019-02-20 00:26:21 +0300

    Added 'home_page' view in 'webnotes_blog/views.py' and associated path for this view in 'webnotes/urls.py'

--- webnotes_blog/urls.py
+++ webnotes_blog/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from webnotes_blog import views as top_level_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', top_level_views.home_page, name='home')
 ]
