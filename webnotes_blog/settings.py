88a324b5b3b4ffd30185d7776dac881f838aa77d
gadjetron <gi87365@yandex.ru>
2019-02-20 00:20:37 +0300

    Added 'content_browser' app to 'INSTALLED_APPS' list. Also configured templates searching path to 'templates/'.

--- webnotes_blog/settings.py
+++ webnotes_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'content_browser.apps.ContentBrowserConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'webnotes_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
