83455673d3f0ac638de79f7442e5f6718e43fb0b
gadjetron <gi87365@yandex.ru>
2019-03-20 20:54:22 +0300

    Added urls for articles loading and searching in 'urls.py' and appropriate functions stubs in 'views.py'

--- content_browser/views.py
+++ content_browser/views.py
@@ -1,2 +1,13 @@
 from django.shortcuts import render
 
+
+def get_article_content(request, id):
+    pass
+
+
+def get_more_articles(request, fetch_count=10):
+    pass
+
+
+def search(request, query_string, fetch_count=10):
+    pass
