8563c97531cfedb6172ba878d2203804b4552ca6
gadjetron <gi87365@yandex.ru>
2019-03-21 01:23:28 +0300

    'home_page' view was rewritten to return last 10 articles from db

--- webnotes_blog/views.py
+++ webnotes_blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from content_browser.models import Article
 
 
 def home_page(request):
-    return render(request, 'index.html')
+    last_10_articles = Article.objects.all()[:10]
+    return render(request, 'index.html', {'last_10_articles': last_10_articles})
