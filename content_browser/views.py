1486231d11b4c03266b2ac54020db25ecc8ae89f
gadjetron <gi87365@yandex.ru>
2019-03-21 01:41:19 +0300

    Added implementation for 'get_article_content' view

--- content_browser/views.py
+++ content_browser/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+from .models import Article
 
 
 def get_article_content(request, id):
-    pass
+    try:
+        article = get_object_or_404(Article, id=id)
+        article_html_string = loader.render_to_string(
+            'content_browser/full_article.html',
+            context={'article': article}
+        )
+    except Article.DoesNotExist:
+        raise Http404("No such article!")
+
+    return HttpResponse(article_html_string)
 
 
 def get_more_articles(request, fetch_count=10):
