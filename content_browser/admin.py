74917c9ac7e581f509d8e039acd1a053dc8b42e1
gadjetron <gi87365@yandex.ru>
2019-03-20 17:50:16 +0300

    Were defined and registered 'Article' and 'Author' models.

--- content_browser/admin.py
+++ content_browser/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Author, Article
 
-# Register your models here.
+admin.site.site_header = "WebNotes Blog"
+admin.site.site_title = "WebNotes Blog admin panel"
+admin.site.index_title = "Site administration panel"
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('header', 'publication_date')
+
+    list_filter = ('publication_date', 'authors')
+
+    date_hierarchy = 'publication_date'
+
+    filter_horizontal = ('authors',)
+
+    search_fields = ['header']
--- content_browser/models.py
+++ content_browser/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+import random
 
-# Create your models here.
+from django.contrib.auth.models import User as UserModel
+from django.db import models
+from django.utils import timezone
+from django.utils.lorem_ipsum import paragraphs as lorem_ipsum_paragraphs
+
+
+class Author(UserModel):
+
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+
+
+def generate_random_article_content():
+    return '\n\n'.join(lorem_ipsum_paragraphs(random.randint(10, 20),
+                                              common=False))
+
+
+class Article(models.Model):
+    header = models.CharField(max_length=150,
+                              unique_for_date="publication_date")
+    content = models.TextField(default=generate_random_article_content)
+    publication_date = models.DateTimeField(default=timezone.now)
+    authors = models.ManyToManyField(Author)
+
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+
+    def __str__(self):
+        return self.header
