6f80f483e285e09af4b4b89b92bd98365f63b873
gadjetron <gi87365@yandex.ru>
2019-03-27 18:16:31 +0300

    Fixed all urls. For 'load_more_search_results' url fixed view name

--- content_browser/urls.py
+++ content_browser/urls.py
@@ -3,13 +3,13 @@ from . import views as cb_views
 
 
 urlpatterns = [
-    re_path(r'^article/(?P<id>\d+)', cb_views.get_article_content,
+    re_path(r'^article/(?P<id>\d+)$', cb_views.get_article_content,
             name="article"),
-    re_path(r'^articles/load_more', cb_views.get_more_articles,
+    re_path(r'^articles/load_more$', cb_views.get_more_articles,
             name="load_more_articles"),
 
-    re_path(r'^search', cb_views.search, name="search"),
-    re_path(r'^search/load_more', cb_views.search,
+    re_path(r'^search$', cb_views.search, name="search"),
+    re_path(r'^search/load_more$', cb_views.search_more,
             name="load_more_search_results")
 ]
 
